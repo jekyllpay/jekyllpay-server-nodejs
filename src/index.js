300b408aa64dd9382767c2d6afa966e82cbbbd2a
Yumin Gui <erichui329@gmail.com>
2019-03-13 19:25:34 -0700

    remove views, build a pure api application

--- src/index.js
+++ src/index.js
@@ -3,52 +3,29 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 
+require('dotenv').config();
+
 const app = new Koa();
 const router = new Router();
 
-require('dotenv').config();
-
 const Helmet = require('koa-helmet')
-const path = require('path');
-
-
-const view = require('koa-ejs');
-
-const serve = require("koa-static");
-const mount = require('koa-mount');
 
 let db = require('./libs/database/db');
 
-
-
 app.use(Helmet());
-
-
-app.use(mount("/assets", serve(__dirname + '/resources/assets')));
-
-
 
 const bodyParser = require('koa-bodyparser');
 app.use(bodyParser());
 
-view(app, {
-  root: path.join(__dirname, 'views'),
-  layout: 'layout',
-  viewExt: 'html',
-  cache: false,
-  debug: false
-});
-
-
 // API routes
 // require('./routes')(router)
-// app.use(router.routes())
-// app.use(router.allowedMethods())
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('home');
+router.get('/', (ctx, next) => {
+  ctx.body = 'Hello world';
 });
 
 app.use(router.routes());
+app.use(router.allowedMethods());
+
 const port = process.env.PORT || 3000;
 app.listen(port);
